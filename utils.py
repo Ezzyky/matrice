a57027a7a4afc777e23a624edfe02e885ae3cee2
Ezzyky <abdellahezzyky@gmail.com>
2026-06-28 21:36:59 +0100

    refactor: add a newline for improved readability in clear_avec_msg function

--- utils.py
+++ utils.py
@@ -4,6 +4,7 @@ def clear_avec_msg(msg="Appuyez sur n'importe quel bouton pour sortir..."):
     print(msg)
     input()
     os.system("cls" if os.name == "nt" else "clear")
+
 def Sauvegarder(matrice1,matrice2):
     with open("file.txt","w") as file:
         file.write(matrice1)
